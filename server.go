b1a86d56757c9a3bdd680f3e861086a9678f291a
Max Houston Ramirez Martel <8385004+soyHouston256@users.noreply.github.com>
2024-04-30 01:36:04 -0500

    feat: middleware

--- server.go
+++ server.go
@@ -3,13 +3,13 @@ package main
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"log"
 	"net/http"
 )
 
 func main() {
 	e := echo.New()
 
-	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
 	e.GET("/", func(c echo.Context) error {
@@ -18,7 +18,7 @@ func main() {
 
 	person := e.Group("/person")
 	person.POST("", create)
-	person.GET("/:id", get)
+	person.GET("/:id", middlewareLogPersons(get))
 	person.PUT("/:id", updated)
 	person.DELETE("/:id", delete)
 
@@ -42,4 +42,11 @@ func delete(c echo.Context) error {
 func get(c echo.Context) error {
 	id := c.Param("id")
 	return c.String(http.StatusOK, "get "+id)
+}
+
+func middlewareLogPersons(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		log.Println("Middleware log persons")
+		return next(c)
+	}
 }
